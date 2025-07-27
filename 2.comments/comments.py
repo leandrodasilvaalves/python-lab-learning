f9efb6d2cb9e20688428ff9552f01a9435d1859a
Leandro Alves <leandro.silva.alves86@gmail.com>
2025-07-27 18:16:33 -0300

    Add initial Python scripts for version, comments, and variables

---
+++ 2.comments/comments.py
@@ -0,0 +1,16 @@
+
+# print("Hello, world!")
+print("Hello, world!")  #This is a comment
+
+# Python does not really have syntax for multiline comments
+# Since python will ignore string literals that are not assigned
+# to a variable, you can add multiline
+# Example:
+
+"""
+This is a comment
+writhen in
+more than just one line
+"""
+
+print("Hello, again!")
---
+++ 3.variables/variables.py
@@ -0,0 +1,17 @@
+x = 5
+y = "John"
+
+print(x)
+print(y)
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
