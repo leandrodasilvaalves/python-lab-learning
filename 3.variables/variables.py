3826124b0a65e3209139a8ee642ba383d7c09247
Leandro Alves <leandro.silva.alves86@gmail.com>
2025-07-29 19:34:53 -0300

    Enhance variable demonstration with additional examples and unpacking

--- 3.variables/variables.py
+++ 3.variables/variables.py
@@ -15,3 +15,23 @@ print(z)
 print(type(x))
 print(type(y))
 print(type(z))
+
+x, y, z = "orange", "banana","cherry"
+
+print(x)
+print(y)
+print(z)
+
+x = x = z = "grape"
+
+print(x)
+print(y)
+print(z)
+
+# unpack a collection
+fruits = ["apple","banana", "cherry"]
+x, y, z = fruits
+
+print(x)
+print(y)
+print(z)
