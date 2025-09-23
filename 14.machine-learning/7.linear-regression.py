0f6660b469839094cbcad3eef2051eb83f71d789
Leandro Alves <leandro.silva.alves86@gmail.com>
2025-09-23 08:42:02 -0300

    Add initial implementation of linear regression example using matplotlib and scipy

---
+++ 14.machine-learning/7.linear-regression.py
@@ -0,0 +1,41 @@
+# https://www.w3schools.com/python/python_ml_linear_regression.asp
+
+import matplotlib.pyplot as plt
+from scipy import stats
+
+def calculdate(x,y):
+    slope, intercept, r, p, std_err = stats.linregress(x, y)
+
+    def myfunc(x):
+        return slope * x + intercept
+
+    ten_years_car_speed = myfunc(10)
+    print('ten years', ten_years_car_speed)
+
+    mymodel = list(map(myfunc, x))
+
+    plt.scatter(x, y)
+    plt.plot(x, mymodel)
+    plt.show()
+
+
+def example_one():
+    x = [5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6]
+    y = [99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86]
+
+    calculdate(x,y)
+
+def example_two():
+    x = [89,43,36,36,95,10,66,34,38,20,26,29,48,64,6,5,36,66,72,40]
+    y = [21,46,3,35,67,95,53,72,58,10,26,34,90,33,38,20,56,2,47,15] 
+
+    calculdate(x,y)
+
+
+example_one()
+example_two()
+
+
+# Revisar esse conteudo com o chatgpt 
+# tentar entender com um pouco mais de detalhes como funciona
+# simular cenários de aplicabilidade, por exemplo, ações, finanças, etc
